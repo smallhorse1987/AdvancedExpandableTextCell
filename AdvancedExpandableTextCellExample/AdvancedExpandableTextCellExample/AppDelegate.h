f0ac2f9030ba35589b44453992580e674e65ea01
chenxiaosong <smallhorse87@163.com>
2016-04-16 11:54:01 +0800

    init commit

---
+++ AdvancedExpandableTextCellExample/AdvancedExpandableTextCellExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdvancedExpandableTextCellExample
+//
+//  Created by chen xiaosong on 16/4/16.
+//  Copyright © 2016年 chen xiaosong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdvancedExpandableTextCellExample/AdvancedExpandableTextCellExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdvancedExpandableTextCellExample
+//
+//  Created by chen xiaosong on 16/4/16.
+//  Copyright © 2016年 chen xiaosong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
